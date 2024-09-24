c39ea4f8e0141f8d487114b0cc0801159731565c
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-09-24 15:32:03 +0300

    Add Final main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:ruvumgt/pages/home_page.dart';
+import 'package:ruvumgt/app_files/home_page.dart';
+//import 'package:ruvumgt/app_files/my_complaints.dart';
+//import 'package:ruvumgt/pages/home_page.dart';
+//import 'package:ruvumgt/app_files/report.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 
-void main() {
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+    await Supabase.initialize(
+    url: 'https://pjjpvczhsjtjqngaufpa.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqanB2Y3poc2p0anFuZ2F1ZnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5MDAzNjEsImV4cCI6MjA0MTQ3NjM2MX0.mrS7XpJx55avElxN4fQeceM-4yihmfd-SBiPxH2HKiU',
+  );
   runApp(const MyApp());
 }
 
